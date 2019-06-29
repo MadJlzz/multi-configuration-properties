dd402fa3ad9edb9b0465462ba64a4e2d41905bc1
Julien Klaer <klaer.j@sfeir.com>
2019-06-29 13:28:50 +0200

    Validation from the Bean instantiation thanks to JB Nizet.   - remarks where made on my own analysis.

--- src/main/java/org/madjlzz/demo/multiconfigurationproperties/configuration/PropertyConfiguration.java
+++ src/main/java/org/madjlzz/demo/multiconfigurationproperties/configuration/PropertyConfiguration.java
@@ -4,18 +4,24 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Configuration
 @EnableConfigurationProperties
 public class PropertyConfiguration {
 
     @Bean
+    @Validated
     @ConfigurationProperties(prefix = "property.first")
     public PropertyExample firstProperty() {
         return new PropertyExample();
     }
 
     @Bean
+    // Do not get fooled by this. It seems the validation is made on the class level and it doesn't
+    // take in account the @ConfigurationProperties.
+    // That means that even if this bean is not annotated, it will be validated too.
+    //@Validated
     @ConfigurationProperties(prefix = "property.second")
     public PropertyExample secondProperty() {
         return new PropertyExample();
